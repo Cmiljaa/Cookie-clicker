2af97c546b7e0e8a9eb83200afa786d20b54a840
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-03-29 23:08:07 +0100

    Second commit
    Added paragraph added everything centered

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 let cookie = document.querySelector('img');
+let p = document.querySelector('p');
 let counter = 0;
 
 cookie.addEventListener('click', () => {
     let title = document.querySelector('title');
+    p.innerText = `${counter} cookies`;
     counter++;
     title.innerText = `${counter} cookies`;
+    
 })
 
 cookie.addEventListener('mouseover', () => {
-    cookie.style.scale = 1.05;
+    cookie.style.scale = 0.9;
 })
 
 cookie.addEventListener('mouseout', () => {
-    cookie.style.scale = 1;
+    cookie.style.scale = 0.8;
 })
