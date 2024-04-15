47340673874fb773336f357d36b4ed7d4bd62c39
Cmiljaa <147316652+Cmiljaa@users.noreply.github.com>
2024-04-15 21:38:31 +0200

    Code is now OOP, sound is removed

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-let cookie = document.querySelector('#cookie');
-let p = document.querySelector('.cookie-section p');
-let autoclicker = document.querySelector('.cookie-section #autoclick');
-let doubleclick = document.querySelector('.cookie-section #doubleclick');
-let reset = document.querySelector('.cookie-section button');
-let add = 1;  let counter = 0; let interval; let bool = true;
+class CookieClicker{
 
-cookie.addEventListener('click', () => update(add))
-
-cookie.addEventListener('mouseover', () => cookie.style.scale = 0.9)
-
-cookie.addEventListener('mousedown',  () => cookie.style.scale = 0.8)
-
-cookie.addEventListener('mouseup', () => cookie.style.scale = 0.9)
-
-cookie.addEventListener('mouseout', () => cookie.style.scale = 0.8)
-
-doubleclick.addEventListener('click', () => add=2)
-
-autoclicker.addEventListener('click', () => {
-    if(bool)
-    {
-        interval = setInterval((intervalid) => {
-            update(1);
-        }, 200);
-        bool = false; 
+    constructor(){
+        this.cookie = document.querySelector('#cookie');
+        this.p = document.querySelector('.cookie-section p');
+        this.autoclicker = document.querySelector('.cookie-section #autoclick');
+        this.doubleclick = document.querySelector('.cookie-section #doubleclick');
+        this.reset = document.querySelector('.cookie-section button');
+        this.add = 1;  this.counter = 0; this.interval; this.bool = true;
+        this.events();
     }
-    else
-        alert("Autoclicker has already started!");
-})
 
-reset.addEventListener('click', () => {
-    add=1;
-    counter = -1;
-    update(add);
-    clearInterval(interval);
-    bool = true;
+    events = () => {
+        this.cookie.addEventListener('click', () => this.update(this.add))
 
-})
+        this.cookie.addEventListener('mouseover', () => this.cookie.style.scale = 0.9)
 
-const update = (i) => {
-    counter+=i;
-    let title = document.querySelector('title');
-    p.innerText = `${counter} cookies`;
-    title.innerText = `${counter} cookies`;
+        this.cookie.addEventListener('mousedown',  () => this.cookie.style.scale = 0.8)
+
+        this.cookie.addEventListener('mouseup', () => this.cookie.style.scale = 0.9)
+
+        this.cookie.addEventListener('mouseout', () => this.cookie.style.scale = 0.8)
+
+        this.doubleclick.addEventListener('click', () => this.add=2)
+
+        this.autoclicker.addEventListener('click', () => {
+            if(this.bool)
+            {
+                this.interval = setInterval((intervalid) => {
+                    this.update(1);
+                }, 200);
+                this.bool = false; 
+            }
+            else
+                alert("Autoclicker has already started!");
+        })
+
+        this.reset.addEventListener('click', () => {
+            this.add=1;
+            this.counter = -1;
+            this.update(this.add);
+            clearInterval(this.interval);
+            this.bool = true;
+        
+        })
+    }
+
+    update = (i) => {
+        this.counter+=i;
+        let title = document.querySelector('title');
+        this.p.innerText = `${this.counter} cookies`;
+        title.innerText = `${this.counter} cookies`;
+    }
+
 }
+
+kolacic = new CookieClicker();
